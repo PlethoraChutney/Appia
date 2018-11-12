11bd1491bc0394e215f969fcbbd2a2ca9cddc93e
Rich <plethoraofchutneys@gmail.com>
2018-11-12 12:53:48 -0800

    Fixed auto_graph deletion bug. Added channel rename, suppressed messages

--- export_script.bat
+++ export_script.bat
@@ -1,15 +1,21 @@
-set date_filename=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
-mkdir "results_%date_filename%"
-move *.arw .\results_%date_filename%
+@echo off
+set date_filename=results_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
+mkdir %date_filename%
+move *.arw .\%date_filename% >nul
 
-python assemble_rename_traces.py .\results_%date_filename%\
+python assemble_rename_traces.py .\%date_filename%\
 set /p descriptive_filename=<temp.txt
-ren results_%date_filename% %descriptive_filename%
+ren %date_filename% %descriptive_filename% >nul
 del temp.txt
 
-copy auto_graph.R .\%descriptive_filename%
-cd .\%descriptive_filename%
-Rscript .\auto_graph.R --no-save --no-restore
-del .\auto_graph.R
-
+if exist %descriptive_filename% (
+  copy auto_graph.R .\%descriptive_filename% >nul
+  cd .\%descriptive_filename%
+  Rscript .\auto_graph.R --no-save --no-restore >nul
+  del .\auto_graph.R
+) else (
+  echo The analyzed trace folder is missing. Exiting without making graphs...
+  pause >nul
+  exit
+)
 exit
