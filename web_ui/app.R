68377dfbc279b32a3c0c690a775f381af1aa4b69
Rich <plethoraofchutneys@gmail.com>
2018-12-13 16:06:34 -0800

    Made no progress on the download button

--- web_ui/app.R
+++ web_ui/app.R
@@ -4,6 +4,8 @@ library(ggplot2)
 
 header.rows <- 2
 
+##### Function Definitions #####
+
 rp.get.header <- function(arw) {
   header <- read_delim(arw, '\t', n_max = 1, col_types = 'cccc')
   head.list <- c(header$SampleName[1], header$Channel[1])
@@ -59,6 +61,8 @@ rp.trace.dir <- function(directory) {
   return(trace.data)
 }
 
+##### Shiny UI #####
+
 trace.data <- NULL
 file.list <- list.dirs('..')
 file.list <- file.list[!str_detect(file.list, pattern = '^../\\.')]
@@ -77,11 +81,13 @@ ui <- fluidPage(
       )
     ),
     mainPanel(
-      plotOutput('tracePlot', height = '800px')
+      plotOutput('tracePlot', height = '800px'),
+      downloadButton('downloadPlotButton', 'Download Plot')
     )
   )
 )
 
+##### Shiny Server #####
 
 server <- function(input, output, session) {
   observeEvent(input$loadData, {
@@ -91,15 +97,25 @@ server <- function(input, output, session) {
     updateCheckboxGroupInput(session, 'channelPicker', 'Pick channel(s)',
                              choices = levels(trace.data$Channel), selected = trace.data$Channel)
 
-  
-  output$tracePlot <- renderPlot({
-    input$loadData
-    
-    trace.data %>% 
-      filter(Sample %in% input$tracePicker & Channel %in% input$channelPicker) %>% 
-      rp.trace.plot()
+    output$tracePlot <- renderPlot({
+      input$loadData
+      
+      trace.data %>% 
+        filter(Sample %in% input$tracePicker & Channel %in% input$channelPicker) %>% 
+        rp.trace.plot()
+    })
   })
-  })  
+  
+  output$downloadPlotButton <- downloadHandler(
+    filename = function() {
+      paste('downloaded.plot', '.pdf')
+    },
+    content = function() {
+      cairo_pdf(filename)
+      last_plot()
+      dev.off()
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
