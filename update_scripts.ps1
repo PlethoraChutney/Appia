460daba1aad98dea992c4990e90a85437b654f1f
HPLC <HPLC@VIBA125.ohsum01.ohsu.edu>
2019-08-09 13:45:13 -0700

    Update the update script to include Alex

--- update_scripts.ps1
+++ update_scripts.ps1
@@ -1,8 +1,8 @@
 echo 'Updating scripts from Script_repos'
-$userDirectories = 'Rich_Results', 'Arpita_Results', 'Sigrid_Results'
+$userDirectories = 'Rich_Results', 'Arpita_Results', 'Sigrid_Results', 'Alex_Results'
 foreach($labmember in $userDirectories)
   {
-    Write-Output 'Updating $labmember'
+    Write-Output "Updating $labmember"
     Copy-Item -path Script_repos\HPLC_Scripts\*.py -destination $labmember -recurse -force -PassThru
     Copy-Item -path Script_repos\HPLC_Scripts\*.R -destination $labmember -recurse -force -PassThru
     Copy-Item -path Script_repos\HPLC_Scripts\*.bat -destination $labmember -recurse -force -PassThru
