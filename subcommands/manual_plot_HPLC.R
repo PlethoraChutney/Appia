62f869369c1bcfce2f2aba999217be64332dd3cd
Rich Posert <rich@posert.com>
2020-03-10 12:52:22 -0700

    Data pipe in manual HPLC

--- subcommands/manual_plot_HPLC.R
+++ subcommands/manual_plot_HPLC.R
@@ -11,28 +11,29 @@ data <- read.csv(file = long_trace_filename, header = TRUE) %>%
   mutate(Normalized = (Signal-min(Signal))/(max(Signal) - min(Signal))) %>%
   ungroup() %>%
   mutate(Channel = if_else(grepl('ex280/em350', Channel), 'Trp',
-                           if_else(grepl('ex488/em509', Channel), 'GFP', as.character(Channel)))) %>% 
+                           if_else(grepl('ex488/em509', Channel), 'GFP', as.character(Channel)))) %>%
   gather(key = Normalized, value = Signal, -Time, -Channel, -Sample)
 
 # 2 Plot ------------------------------------------------------------------
 
-ggplot(data = data, aes(x = Time, y = Signal)) +
-  theme_light() +
-  scale_color_manual(values = c(
-    '#17becf', # cyan
-    '#ff7f0e', # orange
-    '#e377c2', # pink
-    '#1f77b4', # blue
-    '#2ca02c', # green
-    '#d62728', # red
-    '#9467bd', # purple
-    '#7f7f7f', # grey
-    '#bcbd22', # yellow-green
-    '#8c564b',  # brown
-    'dark blue',
-    'black'
-  )) +
-  geom_line(aes(color = Sample)) +
-  facet_grid(Normalized ~ Channel, scales = "free") +
-  xlab("Time (minutes)")
+data %>% 
+  ggplot(aes(x = Time, y = Signal)) +
+    theme_light() +
+    scale_color_manual(values = c(
+      '#17becf', # cyan
+      '#ff7f0e', # orange
+      '#e377c2', # pink
+      '#1f77b4', # blue
+      '#2ca02c', # green
+      '#d62728', # red
+      '#9467bd', # purple
+      '#7f7f7f', # grey
+      '#bcbd22', # yellow-green
+      '#8c564b',  # brown
+      'dark blue',
+      'black'
+    )) +
+    geom_line(aes(color = Sample)) +
+    facet_grid(Normalized ~ Channel, scales = "free") +
+    xlab("Time (minutes)")
 ggsave('fsec_traces.pdf', width = 7, height = 5)
