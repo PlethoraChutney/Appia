cf0b2cf7e76c3e4bef0c69cda8b2d605d978f7b5
Rich Posert <rich@posert.com>
2021-01-07 15:17:39 -0800

    HPLC pathing

--- result_monitor.ps1
+++ result_monitor.ps1
@@ -1,6 +1,6 @@
 # Set folder to watch
 $watcher = New-Object System.IO.FileSystemWatcher
-$watcher.Path = "C:\Users\Rich\Desktop"
+$watcher.Path = "C:\Users\HPLC\Documents\Reports"
 $watcher.Filter = "*.*"
 $watcher.IncludeSubdirectories = $false
 $watcher.EnableRaisingevents = $true
@@ -8,10 +8,17 @@ $watcher.EnableRaisingevents = $true
 # Define action
 $action = {
   $path = $Event.SourceEventArgs.FullPath
-  if ( $path -Match "New" )
-    {
-    D:\Rich\Documents\Scripts\plotly_env/Scripts\python.exe D:\Rich\Documents\Scripts\Appia\appia.py hplc D:\Rich\Documents\Scripts\Appia\HPLC-tests\
-    }
+  if ( $path -Match 'rich' ) {
+    C:\Users\HPLC\AppData\Local\Programs\Python\Python38\python.exe C:\Users\HPLC\Documents\Appia\appia.py hplc C:\Users\HPLC\Documents\Rich_Results
+  } elseif ( $path -Match 'arpita' ) {
+    C:\Users\HPLC\AppData\Local\Programs\Python\Python38\python.exe C:\Users\HPLC\Documents\Appia\appia.py hplc C:\Users\HPLC\Documents\Arpita_Results
+  } elseif ( $path -Match 'alex' ) {
+    C:\Users\HPLC\AppData\Local\Programs\Python\Python38\python.exe C:\Users\HPLC\Documents\Appia\appia.py hplc C:\Users\HPLC\Documents\Alex_Results
+  } elseif ( $path -Match 'james' ) {
+    C:\Users\HPLC\AppData\Local\Programs\Python\Python38\python.exe C:\Users\HPLC\Documents\Appia\appia.py hplc C:\Users\HPLC\Documents\James_Results
+  } elseif ( $path -Match 'kim' ) {
+    C:\Users\HPLC\AppData\Local\Programs\Python\Python38\python.exe C:\Users\HPLC\Documents\Appia\appia.py hplc C:\Users\HPLC\Documents\Kim_Results
+  }
 }
 
 # Which actions should be watched
