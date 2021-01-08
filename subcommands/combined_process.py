eac77e114b1219c036cc3af83e1e60105019d34c
Rich Posert <rich@posert.com>
2021-01-07 16:03:16 -0800

    Globbing actually works

--- subcommands/combined_process.py
+++ subcommands/combined_process.py
@@ -4,14 +4,35 @@ import os
 import argparse
 import logging
 
+def iterFlatten(root):
+    if isinstance(root, (list, tuple)):
+        for element in root:
+            for e in iterFlatten(element):
+                yield e
+    else:
+        yield root
 
 def combined_df(files, h_system):
-    files = [os.path.abspath(x) for x in glob(files)]
-    return files
+    system_extensions = {
+        'waters': '.arw',
+        'shimadzu': '.asc'
+    }
+
+    globbed_files = []
+    for pattern in files:
+        globbed_files.extend(glob(pattern))
+    files = [os.path.abspath(x) for x in globbed_files]
+    for file in files:
+        try:
+            assert file.lower().endswith(('.csv', system_extensions[h_system]))
+        except AssertionError:
+            logging.warning(f'Unexpected file extension in {file}. Did you set the right system?')
+    print(files)
 
 def main(args):
-    cdf = combined_df(args.files, args.system)
-    logging.debug(cdf)
+    flattened_files = list(iterFlatten(args.files))
+    print(flattened_files)
+    cdf = combined_df(flattened_files, str.lower(args.system))
 
 parser = argparse.ArgumentParser(
     description = 'Combined FPLC and HPLC processing',
@@ -21,7 +42,8 @@ parser.set_defaults(func = main)
 parser.add_argument(
     'files',
     help = 'All files to combine and process.',
-    type = str
+    type = str,
+    nargs = '+'
 )
 parser.add_argument(
     '--system',
