770abc4e88ffd795bd457dfa3ef3e1b734b59dcd
Rich <rich@posert.com>
2021-01-20 15:36:19 -0800

    Get R scripts ready for calibrations

--- subcommands/auto_graph_HPLC.R
+++ subcommands/auto_graph_HPLC.R
@@ -15,6 +15,21 @@ data <- read.csv(file = long_trace_filename, header = TRUE) %>%
                            if_else(grepl('ex488/em509', Channel), 'GFP', as.character(Channel)))) %>% 
   filter(Time > 0.5)
 
+cal_10_300 <- tibble(
+    mL = c(3, 7, 14, 22),
+    Size = c(100, 50, 25, 12)
+  )
+cal_5_150 <- tibble(
+    mL = c(0.7, 1.2, 2, 2.1),
+    Size = c(100, 50, 25, 12)
+  )
+
+if( max(data$mL) > 20) {
+  calibrations <- cal_10_300
+} else {
+  calibrations <- cal_5_150
+}
+
 # 2 Plot ------------------------------------------------------------------
 
 if (length(levels(as.factor(data$Sample))) > 12) {
@@ -37,23 +52,38 @@ if (length(levels(as.factor(data$Sample))) > 12) {
                                               )
 }
                        
-
-ggplot(data = data, aes(x = mL, y = Signal)) +
+data %>% 
+  ggplot(aes(x = mL, y = Signal)) +
   theme_minimal() +
-  color_scheme +
-  geom_line(aes(color = Sample)) +
-  facet_grid(rows = vars(Channel), scales = "free") +
-  ggtitle(str_c(experiment_name, ' Raw Signal')) +
-  xlab("Volume (mL)")
-ggsave('fsec_traces.pdf', width = 7, height = 5)
-
-ggplot(data = data, aes(x = mL, y = Normalized)) +
-  theme_minimal() +
+  geom_vline(data = calibrations, aes(xintercept = mL), color = 'grey', linetype = 'dashed') +
+  scale_x_continuous('Volume (mL)', 
+                     sec.axis = sec_axis(trans = ~.,
+                                         name = 'Calibrations (MDa)',
+                                         breaks = calibrations$mL,
+                                         labels = calibrations$Size)
+  ) +
+  theme(axis.title.x.top = element_text(size = 8)) +
   color_scheme +
   geom_line(aes(color = Sample)) +
   facet_grid(rows = vars(Channel), scales = "free") +
   xlab("Volume (mL)") +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.25)))
+ggsave('fsec_traces.pdf', width = 7, height = 5)
+
+data %>% 
+  ggplot(aes(x = mL, y = Normalized)) +
+  theme_minimal() +
+  geom_vline(data = calibrations, aes(xintercept = mL), color = 'grey', linetype = 'dashed') +
+  scale_x_continuous('Volume (mL)', 
+                     sec.axis = sec_axis(trans = ~.,
+                                         name = 'Standards (MDa)',
+                                         breaks = calibrations$mL,
+                                         labels = calibrations$Size)
+                     ) +
+  theme(axis.title.x.top = element_text(size = 8)) +
+  color_scheme +
+  geom_line(aes(color = Sample)) +
+  facet_grid(rows = vars(Channel), scales = "free") +
   coord_cartesian(ylim = c(0, 1)) +
-  ggtitle(str_c(experiment_name, ' Normalized Signal')) +
   ylab("Normalized Signal")
 ggsave('normalized_traces.pdf', width = 7, height = 5)
--- subcommands/manual_plot_HPLC.R
+++ subcommands/manual_plot_HPLC.R
@@ -9,6 +9,21 @@ long_trace_filename <- "long_chromatograms.csv"
 data <- read.csv(file = long_trace_filename, header = TRUE) %>% 
   pivot_longer(cols = c(Signal, Normalized), names_to = 'Normalization', values_to = 'Signal')
 
+cal_10_300 <- tibble(
+  mL = c(3, 7, 14, 22),
+  Size = c(100, 50, 25, 12)
+)
+cal_5_150 <- tibble(
+  mL = c(0.7, 1.2, 2, 2.1),
+  Size = c(100, 50, 25, 12)
+)
+
+if( max(data$mL) > 20) {
+  calibrations <- cal_10_300
+} else {
+  calibrations <- cal_5_150
+}
+
 # 2 Plot ------------------------------------------------------------------
 
 if (length(levels(as.factor(data$Sample))) > 12) {
@@ -36,8 +51,15 @@ data %>%
   filter(Time > 0.5) %>% 
   ggplot(aes(x = mL, y = Signal)) +
   theme_minimal() +
+  geom_vline(data = calibrations, aes(xintercept = mL), color = 'grey', linetype = 'dashed') +
+  scale_x_continuous('Volume (mL)', 
+                     sec.axis = sec_axis(trans = ~.,
+                                         name = 'Calibrations (MDa)',
+                                         breaks = calibrations$mL,
+                                         labels = calibrations$Size)
+  ) +
+  theme(axis.title.x.top = element_text(size = 8)) +
   color_scheme +
   geom_line(aes(color = Sample)) +
-  facet_grid(Normalization ~ Channel, scales = "free") +
-  xlab("Volume (mL)")
+  facet_grid(Normalization ~ Channel, scales = "free")
 ggsave('fsec_traces.pdf', width = 7, height = 5)
