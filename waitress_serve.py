8fce1369789e5f2f312768340e93774a2773f6c3
Rich <rich@posert.com>
2021-05-19 16:27:19 -0700

    Fix waitress timing for docker

--- waitress_serve.py
+++ waitress_serve.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
+import socket
+import os
+import logging
+from time import sleep
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    result = sock.connect_ex((os.environ['COUCHDB_HOST'], 5984))
+except socket.error:
+    logging.debug('Server refused connection')
+    result = 1
+
+while result != 0:
+    logging.debug('CouchDB port not open')
+    sleep(5)
+    try:
+        result = sock.connect_ex((os.environ['COUCHDB_HOST'], 5984))
+    except socket.error:
+        logging.debug('Server refused connection')
+        result = 1
+
+logging.info('Port is open')
+
 
 from waitress import serve
 from web import server
-from time import sleep
+import logging
 
-while True:
-    try:
-        serve(server, listen='0.0.0.0:8080')
-    except ConnectionRefusedError:
-        sleep(5)
+serve(server, listen='0.0.0.0:8080', ipv6 = False)
