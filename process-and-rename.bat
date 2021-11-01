cbdfe55ce046169b592fc959c3188d8894efd6a3
Rich <rich@posert.com>
2021-11-01 10:54:18 -0700

    Update batch files

--- process-and-rename.bat
+++ process-and-rename.bat
@@ -2,7 +2,7 @@
 
 set /P new_name="Rename experiment to: "
 
-python appia.py -v process ./* --id "%new_name%" --output-dir "%new_name%" --database
+python appia.py --ignore-gooey process ./* --id "%new_name%" --output-dir "%new_name%" --database
 
 if %ERRORLEVEL%==1 (
     PAUSE
