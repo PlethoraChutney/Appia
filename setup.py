6ef33c4b76067267f7901698aac99569b1e7257e
Rich <rich@posert.com>
2022-12-15 11:35:52 -0800

    pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name = 'appia',
-    version = '7.1.0',
+    version = '7.1.1',
     author = 'Rich Posert',
     author_email = 'posert@ohsu.edu',
     description = 'Chromatography processing made easy',
