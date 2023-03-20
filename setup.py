125a260073be62d2c8dd07d9479104dc6694da6f
Rich <rich@posert.com>
2023-03-20 16:25:02 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name = 'appia',
-    version = '7.2.4',
+    version = '7.3.0',
     author = 'Rich Posert',
     author_email = 'posert@ohsu.edu',
     description = 'Chromatography processing made easy',
